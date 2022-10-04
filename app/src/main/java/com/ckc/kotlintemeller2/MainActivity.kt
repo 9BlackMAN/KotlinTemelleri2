1fcf2b216e7774f72a669519905abbb6e6881d34
ckc <can.cakici.91@gmail.com>
2022-10-04 15:45:50 +0300

    second commit

--- app/src/main/java/com/ckc/kotlintemeller2/MainActivity.kt
+++ app/src/main/java/com/ckc/kotlintemeller2/MainActivity.kt
@@ -7,5 +7,51 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var sample = Sample(54,"can","can")
+        sample.age = 5
+        sample.name = "can"
+        sample.surName = "can"
+        sample.sampleFun()
+
+
+
+        //Nullability
+        // Nullable (?) && Non-null
+        // !! vs ?
+
+        var myString : String? = null
+        var myAge : Int? = null
+
+
+        //1)Null safety
+
+        if (myAge != null) {
+            println(myAge * 10)
+        } else {
+            println("myAge null")
+        }
+
+        //2)safe call
+
+        println(myAge?.compareTo(2))
+
+        //3)elvis
+
+        val myResult = myAge?.compareTo(2) ?: -100
+        println(myResult)
+
+
+
+    }
+    fun sum(a:Int,b:Int):Int{
+
+        return a+b
+    }
+    fun test(){
+        println("bu bir testtir")
+    }
+    fun sample2(a:String){
+        
     }
 }
---
+++ app/src/main/java/com/ckc/kotlintemeller2/Sample.kt
@@ -0,0 +1,30 @@
+package com.ckc.kotlintemeller2
+
+class Sample(var age:Int,var name:String,var surName:String) { //kısa Constractor yapılışı
+
+
+/*
+//Property
+    var age = 0
+    var name = ""
+    var surName = ""
+    */
+
+/*
+//Bu yöntem zor
+    constructor(age:Int,name: String, surName:String){
+
+        this.age=age
+        this.name= name
+        this.surName=surName
+    }
+
+ */
+
+    fun sampleFun(){
+        println("Hello people")
+    }
+
+
+
+}
